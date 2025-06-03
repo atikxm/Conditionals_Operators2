865a372e1fbf099f0f63adb5429346157011e33a
atikxm <atikurrahmanxm@gmail.com>
2025-06-04 02:19:02 +0530

    real-life practice problem Number Boro-Chhoto and soman Check

--- Conditionals_Operators2.py
+++ Conditionals_Operators2.py
@@ -1,3 +1,4 @@
+'''
 name = input("Tumar naam ki? ")
 age = int(input("Boyosh koto? "))
 
@@ -5,3 +6,15 @@ if name.lower() == "alien" and age >= 18:
     print("Welcome, boss alien!")
 else:
     print("Access Denied.")
+'''
+
+
+num1=int (input ("First  number :"))
+num2=int (input ("Second number :"))
+if num1>num2:
+    print("First number boro second number ar theke .")
+elif num1<num2:
+    print("Second number boro first number ar theke .")
+else:
+    print("Duita number soman .")
+    
