de4fab6fb9b3f6d933f7ed7546f1e488e7c22423
atikxm <atikurrahmanxm@gmail.com>
2025-06-04 02:05:38 +0530

    simple assignments in conditionals opertaors

---
+++ Conditionals_Operators2.py
@@ -0,0 +1,7 @@
+name = input("Tumar naam ki? ")
+age = int(input("Boyosh koto? "))
+
+if name.lower() == "alien" and age >= 18:
+    print("Welcome, boss alien!")
+else:
+    print("Access Denied.")
