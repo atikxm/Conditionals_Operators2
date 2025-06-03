3970ba4aeca19fec743a912982a4251cd5857cb7
atikxm <atikurrahmanxm@gmail.com>
2025-06-04 02:39:08 +0530

    Result System

--- Conditionals_Operators2.py
+++ Conditionals_Operators2.py
@@ -8,7 +8,7 @@ else:
     print("Access Denied.")
 '''
 
-
+'''
 num1=int (input ("First  number :"))
 num2=int (input ("Second number :"))
 if num1>num2:
@@ -17,4 +17,19 @@ elif num1<num2:
     print("Second number boro first number ar theke .")
 else:
     print("Duita number soman .")
+
+'''
+marks = int(input("Enter your marks: "))
+if marks >= 80:
+    print("You got A+")
+elif marks >= 70 :
+    print("Yoy got A")
+elif marks >=60:
+    print("You got A-")
+elif marks >= 50:
+    print("You got B")
+elif marks >= 40:
+    print(" Failed")
+else:
+    print("You are failed")
     
